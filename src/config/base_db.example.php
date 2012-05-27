d30d5a96ff64b51f156f1542926e0e5bb1cc34dc
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:35:24 +0100

    Base DB Config File

---
+++ src/config/base_db.example.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * All entries are required.
+ * 
+ * This user only needs read access to the db (it will take a dump of it)
+ */
+
+$dbhost = 'localhost';
+$dbport = '3306';
+$dbname = 'databasenname';
+$dbuser = 'username';
+$dbpasswd = 'password';
+
+?>
