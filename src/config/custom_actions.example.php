b8cd790c040cdf2e340c7231b6b454037d18d2bc
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:42:37 +0100

    Add custom actions config

--- src/config/custom_actions.example.php
+++ src/config/custom_actions.example.php
@@ -1,1 +1,13 @@
 <?php
+
+/**
+ * Add all queries you wish to execute into the $queries array.
+ */
+
+$queries = array(); // Don't touch this
+
+$queries += array('Blah');
+$queries += array('Blah');
+$queries += array('Blah');
+
+?>
