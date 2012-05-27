54ee07a7f51fb7b8b911ede9ecbf8381727ab13e
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:56:35 +0100

    Setup common.php

--- src/common.php
+++ src/common.php
@@ -1,2 +1,33 @@
 <?php
+/**
+*
+* @package update_db
+* @copyright (c) 2012 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
 
+if (!defined(IN_UPDATE))
+{
+	exit;
+}
+
+if (file_exists($root_path . 'custom_common' . $phpEx))
+{
+	include($root_path . 'custom_common' . $phpEx);
+}
+
+include($root_path . 'config/base_db' . $phpEx);
+include($root_path . 'config/target_db' . $phpEx);
+include($root_path . 'config/custom_actions' . $phpEx);
+
+include($root_path . 'includes/update_db' . $phpEx);
+
+$custom_actions = $queries;
+unset($queries);
+
+$update_db = new update_db;
+
+// Connect to base DB
+$update_db->connect($base, $basehost, $baseport, $basename, $baseuser, $basepasswd);
+$update_db->connect($target, $targethost, $targetport, $targetname, $targetuser, $targetpasswd);
