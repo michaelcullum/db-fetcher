72c77a2ef53ef3687a3a967a222246e965c23911
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:37:39 +0100

    Adding target db config file

--- src/config/target_db.example.php
+++ src/config/target_db.example.php
@@ -1,1 +1,15 @@
 <?php
+
+/**
+ * All entries are required.
+ * 
+ * This user needs full access to the repo and global access to create, edit and drop tables.
+ */
+
+$dbhost = 'localhost';
+$dbport = '3306';
+$dbname = 'databasenname';
+$dbuser = 'username';
+$dbpasswd = 'password';
+
+?>
