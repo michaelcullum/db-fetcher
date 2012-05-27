6cf17a0d03b3a5ed9c3f43d867481d99c1a30d84
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 17:16:00 +0100

    Quite a bit more work, bit shabby but as long as it works I dont care.

--- src/includes/update_db.php
+++ src/includes/update_db.php
@@ -15,6 +15,7 @@ class update_db
 	protected $target_db_name;
 	protected $root_path;
 	protected $phpEx;
+	protected $tables;
 
 	public function __construct ($root_path, $phpEx)
 	{
@@ -54,6 +55,64 @@ class update_db
 	public function unconnect()
 	{
 		mysql_close($this->base_connection);
+	}
+
+	public function fetch_and_replace_process($method)
+	{
+		if ($this->fetch_base && $this->replace_target)
+		{
+			echo 'Complete, no errors';
+		}
+	}
+
+	public function fetch_base()
+	{
+		$this->tables = mysql_list_tables($this->base_db_name, $this->base_connection);
+
+		foreach ($this->tables as $table) 
+		{
+			mysql_select_db($this->base_db_name);
+			$backup_file = $this->root_path . 'data/dumps/' . time() . '_' . $table .'.sql';
+			$query      = "SELECT * INTO OUTFILE '$backupFile' FROM " . $table;
+			$result = mysql_query($query);
+		}
+	}
+
+	public function replace_target()
+	{
+
+	}
+
+	public function custom_actions()
+	{
+
+	}
+
+	public function drop_target()
+	{
+		if (mysql_drop_db($this->target_db_name, $this->target_connection))
+		{
+			echo 'DB Dropped';
+		}
+	}
+
+	public function create_db()
+	{
+		if (mysql_create_db($this->target_db_name, $this->target_connection))
+		{
+			echo 'DB Created';
+		}
+	}
+
+	public function import_sql()
+	{
+		foreach ($this->tables as $table) 
+		{
+			mysql_select_db($this->target_db_name);
+			$backup_file = $this->root_path . 'data/dumps/' . time() . '_' . $table .'.sql';
+			$query      = "LOAD DATA INFILE '$backupFile' INTO TABLE " . $table;
+			$result = mysql_query($query);
+		}
 	}
 }
 
