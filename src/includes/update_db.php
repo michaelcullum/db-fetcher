7a2811da53eed1e6bffa535277a3da329daa6e1c
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:57:10 +0100

    Start a class

--- src/includes/update_db.php
+++ src/includes/update_db.php
@@ -1,8 +1,21 @@
 <?php
+/**
+*
+* @package update_db
+* @copyright (c) 2012 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
 
 class update_db
 {
-	
+	protected $base_connection;
+	protected $target_host;
+	protected $target_db_name;
+	protected $target_user;
+	protected $target_passwd;
+
+
 }
 
 ?>
