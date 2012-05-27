ca88d1c4459a4b11a9ba65417b42c52b29e262ba
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-05-27 15:57:30 +0100

    Basics, include common.php etc.
    CLI will come later

--- src/update_db.php
+++ src/update_db.php
@@ -1,1 +1,15 @@
 <?php
+/**
+*
+* @package update_db
+* @copyright (c) 2012 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+$root_path = './';
+$phpEx = '.php'
+
+define(IN_UPDATE, true);
+
+include($root_path . 'common' . $phpEx);
